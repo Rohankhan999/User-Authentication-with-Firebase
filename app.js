aeab92b3429157e56d94e0180ac880399ca8705e
Rohankhan999 <rohankhan08453@gmail.com>
2024-12-27 19:13:15 +0500

    .

---
+++ app.js
@@ -0,0 +1,14 @@
+<script type="module">
+  import { initializeApp } from "https:
+    const firebaseConfig = {
+    apiKey: "AIzaSyAy2sdXZBATUG899y7KZpZsnOluIckd_Jc",
+    authDomain: "login-signup-999.firebaseapp.com",
+    projectId: "login-signup-999",
+    storageBucket: "login-signup-999.firebasestorage.app",
+    messagingSenderId: "831110980438",
+    appId: "1:831110980438:web:0974f24c997274899d2cf7"
+  };
+
+  
+  const app = initializeApp(firebaseConfig);
+</script>
