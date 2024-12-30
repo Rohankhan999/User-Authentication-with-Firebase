fa70a6a9d5d1813661a630b2a8de0915cd8bb64f
Rohankhan999 <rohankhan08453@gmail.com>
2024-12-30 15:30:10 +0500

    /

--- app.js
+++ app.js
@@ -11,17 +11,43 @@ const firebaseConfig = {
 
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
-function signup(){
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-  createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log("Data -->",user);
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      console.log("Error -->",errorMessage);
-    });
-  }
-  signup();
+
+  function signup() {
+    const email = document.getElementById("Email").value;
+    const password = document.getElementById("Password").value;
+
+    if (!email || !password) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Please enter an Email and Password",
+        });
+        return;
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((res) => {
+            const user = res.user;
+            console.log("Data -->", user);
+
+            Swal.fire({
+                title: "Congratulations!!",
+                text: "You have successfully signed up.",
+                icon: "success",
+                draggable: true,
+            }).then(() => {
+                window.open("login.html", "_self");
+            });
+        })
+        .catch((error) => {
+            const errorMessage = "Something went wrong. Please try again.";
+            console.log("Error -->", errorMessage);
+            Swal.fire({
+                icon: "error",
+                title: "Sign Up Failed",
+                text: errorMessage,
+            });
+        });
+}
+
+document.getElementById("submit").onclick = signup;
